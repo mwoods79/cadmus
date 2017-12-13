d922d475bbc9f02f607bb47cc688935f970c4768
Micah Woods <micahwoods@gmail.com>
2017-12-12 22:08:47 -0500

    2. Solutions to problem 1 and 2 of Router workshop

--- lib/cadmus/router.ex
+++ lib/cadmus/router.ex
@@ -1,22 +1,38 @@
 defmodule Cadmus.Router do
-  # 1. Change this to an import. This will allow the use of `Cadmus.Conn`
-  # functions without needing to namespace.
-  alias Cadmus.Conn
+  # 0. Optionally you can change this to an import. This will allow the use of
+  # Cadmus.Conn functions without needing to namespace.
+  import Cadmus.Conn
 
   def call(conn) do
     conn
-    |> Conn.put_status(200)
-    |> Conn.put_resp_body("<h1>Hello World!</h1>")
+    |> put_status(200)
+    |> put_resp_body("<h1>Hello World!</h1>")
+    match(conn, conn.method, conn.path)
   end
 
   ## 1. Use the function below in the call method.
   def match(conn, "GET", "/hello") do
-    # conn
-    # |> put_status(200)
-    # |> put_resp_body("<h1>Hello World!</h1>")
+    conn
+    |> put_status(200)
+    |> put_resp_body("<h1>Hello World!</h1>")
   end
 
   ## 2. Continue to use pattern matching until all the tests pass
   def match(conn, "GET", "/dogs") do
+    conn
+    |> put_status(200)
+    |> put_resp_body("<ul><li>mongo</li><li>chubbs</li></ul>")
+  end
+
+  def match(conn, "POST", "/dogs") do
+    conn
+    |> put_status(201)
+    |> put_resp_body("<ul><li>#{conn.query_params[:name]}</li></ul>")
+  end
+
+  def match(conn, _method, _path) do
+    conn
+    |> put_status(404)
+    |> put_resp_body("<h1>Not Found Error</h1>")
   end
 end
